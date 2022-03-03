9c7d96ed01c5aa3eb186cfa2e6411446deb6a6c6
Harald Ellingsen <he@pioneer-robotics.no>
2022-03-03 07:43:21 +0100

    fixes

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from datetime import datetime, date, timedelta
-from email import header
-from time import sleep, time
-
-from black import out
+import decimal
+from distutils.log import debug
 
 def parse_jobblogg(file_name, with_comments=True):
   with open(file_name, encoding='utf-8', mode='r') as f:
@@ -22,6 +20,7 @@ def parse_jobblogg(file_name, with_comments=True):
   
 def format_entries(list_of_entries):
   entries = []
+  debugprint = False
   for id, one_entry  in enumerate(list_of_entries):
     entry_datetime_from = one_entry.get("datetime")  
     time_from = entry_datetime_from.time()
@@ -30,6 +29,7 @@ def format_entries(list_of_entries):
     time_to = 0
     duration = 0
     entry_datetime_to = 0
+    duration_total_seconds = 0
     
     if id < len(list_of_entries) - 1:
       entry_datetime_to = list_of_entries[id+1].get("datetime")
@@ -37,12 +37,20 @@ def format_entries(list_of_entries):
 
     if entry_datetime_to != 0:
       duration = entry_datetime_to - entry_datetime_from
+      duration_total_seconds = duration.total_seconds()
+    
+    
+    if not debugprint:
+      # print(f"type of duration: {type(duration)} methods: {dir(duration)} ")
+      # print(f"total_seconds: {duration.total_seconds()}")
+      debugprint = True
     
     entries.append({"date": date_from,
                     "details":
                     {"from": time_from,
                     "to": time_to,
                     "duration": duration,
+                    "duration_decimal": decimal.Decimal(duration_total_seconds/3600),
                     "description": description}
                     })
   return entries
@@ -57,8 +65,8 @@ def print_filtered_entries(entries, date_filter_from, date_filter_to, csv=False)
       if last_weekday != current_weekday:
         print(f"\n{'#'*50}\n{entry_date} {weekdays[current_weekday]}\n{'#'*50}\n")
         keys = list(i.get('details').keys())
-        print(f"{keys[0]+':':<15}{keys[1]+':':<15}{keys[2]+':':<15}{keys[3]+':'}")
-      print(f"{str(i.get('details').get('from')):<15}{str(i.get('details').get('to')):<15}{str(i.get('details').get('duration')):<15}{i.get('details').get('description')}")
+        print(f"{keys[0]+':':<15}{keys[1]+':':<15}{keys[2]+':':<15}{keys[3]+':':<20}{keys[4]+':'}")
+      print(f"{str(i.get('details').get('from')):<15}{str(i.get('details').get('to')):<15}{str(i.get('details').get('duration')):<15}{float(i.get('details').get('duration_decimal')):<20.2f}{i.get('details').get('description')}")
       last_weekday = current_weekday
       
       
@@ -67,7 +75,7 @@ def main():
   jobb_logg_file_name=r'C:\Users\HaraldEllingsen\iCloudDrive\iCloud~is~workflow~my~workflows\jobb-logg2.txt'
   entry_list = parse_jobblogg(jobb_logg_file_name, with_comments=True)
   
-  date_from = date(2022, 2, 14)
+  date_from = date(2022, 2, 21)
   date_to = date_from + timedelta(days=7)
   
   print("Processing: " + jobb_logg_file_name)
